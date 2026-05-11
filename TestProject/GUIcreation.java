5fc6de97ebae67f6c58555e10d6cbc1369da0973
Evan <mereva29@ecolecatholique.ca>
2026-05-11 11:09:50 -0400

    checkboxes

--- TestProject/GUIcreation.java
+++ TestProject/GUIcreation.java
@@ -6,20 +6,33 @@ public class GUIcreation extends JFrame {
 	JPanel topPanel = new JPanel();
 	JPanel leftPanel = new JPanel();
 	JPanel bottomPanel = new JPanel();
+	JCheckBox box1 = new JCheckBox("Plug");
+	JCheckBox box2 = new JCheckBox("Cable");
+	JCheckBox box3 = new JCheckBox("Ecran");
+	JCheckBox box4 = new JCheckBox("Batterie");
+	JCheckBox box5 = new JCheckBox("Allume/Eteint");
+	JCheckBox box6 = new JCheckBox("Logiciel");
+	JCheckBox[] boxCateg = {box1, box2, box3, box4, box5, box6};
+
 	public GUIcreation () {
 		super ("GUIcreation");
 		setSize(500,500);
 		getContentPane().setLayout(new GridBagLayout());
-		
 		GridBagConstraints gbc = new GridBagConstraints();
 	    gbc.fill = GridBagConstraints.BOTH;
 	    gbc.weightx = 0.5;
 	    gbc.weighty = 0.5;
 	    gbc.gridx = 0;
 	    gbc.gridy = 0;
-		rightPanel.setLayout(new GridBagLayout());
+		leftPanel.setLayout(new GridBagLayout());
+		for(int i=0; i<6; i++) {
+			leftPanel.add(boxCateg[i], gbc);
+			gbc.gridy = i+1;
+		}
+		gbc.gridy = 0;
 		this.add(leftPanel, gbc);
 		gbc.gridx = 1;
+		rightPanel.setLayout(new GridBagLayout());
 		this.add(rightPanel, gbc);
 	    gbc.gridx = 0;
 	    topPanel.setBackground(Color.black);
