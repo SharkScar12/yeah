cc8e7b43a6e38088ddcf7443a514962a875a2318
Evan <mereva29@ecolecatholique.ca>
2026-05-11 11:55:20 -0400

    guicreation work

--- TestProject/GUIcreation.java
+++ TestProject/GUIcreation.java
@@ -3,9 +3,11 @@ import javax.swing.*;
 import java.awt.event.*;
 public class GUIcreation extends JFrame {
 	JPanel rightPanel = new JPanel();
-	JPanel topPanel = new JPanel();
+	JPanel rightTopPanel = new JPanel();
 	JPanel leftPanel = new JPanel();
-	JPanel bottomPanel = new JPanel();
+	JPanel leftTopPanel = new JPanel();
+	JPanel leftBottomPanel = new JPanel();
+	JPanel rightBottomPanel = new JPanel();
 	JCheckBox box1 = new JCheckBox("Plug");
 	JCheckBox box2 = new JCheckBox("Cable");
 	JCheckBox box3 = new JCheckBox("Ecran");
@@ -13,6 +15,9 @@ public class GUIcreation extends JFrame {
 	JCheckBox box5 = new JCheckBox("Allume/Eteint");
 	JCheckBox box6 = new JCheckBox("Logiciel");
 	JCheckBox[] boxCateg = {box1, box2, box3, box4, box5, box6};
+	JTextPane categories = new JTextPane();
+	JTextPane description = new JTextPane();
+	JTextPane descriptionInst = new JTextPane();
 
 	public GUIcreation () {
 		super ("GUIcreation");
@@ -24,24 +29,43 @@ public class GUIcreation extends JFrame {
 	    gbc.weighty = 0.5;
 	    gbc.gridx = 0;
 	    gbc.gridy = 0;
-		leftPanel.setLayout(new GridBagLayout());
+	    leftPanel.setLayout(new GridBagLayout());
+	    this.add(leftPanel, gbc);
+	   
+		leftTopPanel.setLayout(new GridBagLayout());
+		categories.setText("Cochez le(s) boîte(s) applicable(s) à votre problème:");
+		categories.setEditable(false);
+	    leftTopPanel.add(categories, gbc);
 		for(int i=0; i<6; i++) {
-			leftPanel.add(boxCateg[i], gbc);
+			leftTopPanel.add(boxCateg[i], gbc);
 			gbc.gridy = i+1;
 		}
 		gbc.gridy = 0;
-		this.add(leftPanel, gbc);
+		leftPanel.add(leftTopPanel, gbc);
+		
+		gbc.gridy = 1;
+		gbc.weighty = 0.02;
+		descriptionInst.setText("Entrez une description:");
+		descriptionInst.setEditable(false);
+		leftPanel.add(descriptionInst, gbc);
+		
+		gbc.gridy = 2;
+		gbc.weighty = 0.2;
+		leftPanel.add(description, gbc);
+		gbc.gridy = 0;
+		gbc.weighty = 0.5;
 		gbc.gridx = 1;
+		
 		rightPanel.setLayout(new GridBagLayout());
 		this.add(rightPanel, gbc);
 	    gbc.gridx = 0;
-	    topPanel.setBackground(Color.black);
-		rightPanel.add(topPanel, gbc);
+	    rightTopPanel.setBackground(Color.black);
+		rightPanel.add(rightTopPanel, gbc);
 		gbc.gridy = 1;
 		gbc.weighty = 0.15;
-		bottomPanel.setBackground(Color.blue);
-		rightPanel.add(bottomPanel, gbc);
-
+		rightBottomPanel.setBackground(Color.blue);
+		rightPanel.add(rightBottomPanel, gbc);
+		
 		setVisible(true);
 	}
 }
