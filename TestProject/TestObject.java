9f1e84469fdfa3aa36fd217ce900cc1011f0e8f5
PaulD <joe@example.com>
2026-05-11 10:45:07 -0400

    Push new stuff

--- TestProject/TestObject.java
+++ TestProject/TestObject.java
@@ -1,5 +1,7 @@
+//import org.apache.commons.validator.routines.EmailValidator;
 
 public class TestObject {
+	System.out.println("This");
 //eeeee
 //pls stop
 }
--- TestProject/TicketReparation.java
+++ TestProject/TicketReparation.java
@@ -1,3 +1,11 @@
+import java.io.BufferedWriter;
+import java.io.File;
+import java.io.FileWriter;
+import java.io.IOException;
+import java.io.PrintWriter;
+import java.util.Scanner;
+import java.io.FileNotFoundException;
+
 public class TicketReparation extends CompteGUI {
 /*
 afficheInfo(boolean)
@@ -6,47 +14,78 @@ envoyerEmail() : String
 */
 
   private static int totalTicket;
-  private int ticketID;
+  private String ticketID;
   private String description;
   private boolean[] categories;
   private boolean affichage;
+  File ficherNoTicket;
   
   //Constructeurs de CompteGUI
   TicketReparation() {
     super();
-    this.totalTicket++;
-    this.ticketID = totalTicket;
+    try {
+      ficherNoTicket = new File("ticketTotal.txt");
+      Scanner lecteurTotal = new Scanner(ficherNoTicket);
+      totalTicket = Integer.parseInt(lecteurTotal.next());
+      lecteurTotal.close();
+    } catch (FileNotFoundException f) {
+      f.printStackTrace();
+      System.out.println("Fichier non-retrouver");
+    }
+    this.ticketID = "i0";
     this.description = "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.";
     this.categories = new boolean[] 
       {false, false, false, false, false, false, false, false};
     this.affichage = true;
   }
   
-  TicketReparation(int compteE, String emailE, String descriptionE, boolean[] categoriesE) {
+  TicketReparation(String ticketE, int compteE, String emailE, String descriptionE, boolean[] categoriesE, boolean afficheE) {
     super(compteE, emailE);
-    this.totalTicket++;
-    this.ticketID = totalTicket;
+    try {
+      ficherNoTicket = new File("ticketTotal.txt");
+      Scanner lecteurTotal = new Scanner(ficherNoTicket);
+      totalTicket = Integer.parseInt(lecteurTotal.next());
+      lecteurTotal.close();
+    } catch (FileNotFoundException f) {
+      f.printStackTrace();
+      System.out.println("Fichier non-retrouver");
+    }
+    
+    this.ticketID = ticketE;
     this.description = descriptionE;
     this.categories = categoriesE;
-    this.affichage = true;
+    this.affichage = afficheE;
   }
   
   //Méthodes accesseurs et mutateurs
   //totalTicket
   public int getTotalTicket() {
-    return this.totalTicket;
+    return totalTicket;
   }
   
   public void setTotalTicket(int ticketE) {
-    this.totalTicket = ticketE;
+    totalTicket = ticketE;
+  }
+  
+  public void upTotalTicket() {
+    totalTicket++;
+    try {
+      PrintWriter writerTemp = new PrintWriter(new BufferedWriter(new FileWriter(ficherNoTicket)));
+      writerTemp.println(totalTicket);
+      writerTemp.close();
+    } catch (IOException e) {
+      System.out.println("Il y a eu une erreur.");
+		    e.printStackTrace();
+    }
+    
   }
   
   //ticketID
-  public int getTicketID() {
+  public String getTicketID() {
     return this.ticketID;
   }
   
-  public void setTicketID(int ticketE) {
+  public void setTicketID(String ticketE) {
     this.ticketID = ticketE;
   }
   
@@ -81,5 +120,31 @@ envoyerEmail() : String
     this.affichage = affichageE;
   }
 
+  public void systemeAffiche () {
+    System.out.println("Information du ticket : " + ticketID);
+    System.out.println("ID du compte : " + super.getCompteID() + "\nEmail du compte : " + super.getEmail() +  "\nDescription : " + description + "\nCategories : " + categories);
+  }
+
+   
+  public void creerTicket() {
+    try {
+      	//Écrit les informations dans tickets.txt
+        upTotalTicket();
+        File ficherTicket = new File("tickets.txt");
+        PrintWriter monWriter = new PrintWriter(new BufferedWriter(new FileWriter(ficherTicket, true)));
+        //Separe les valeurs du tableau de categories en un string du format "valeaur";"valeur";.... pour la documentation dans le fichier
+        String categStr = "";
+        for (int i = 0; i < categories.length; i++) {
+          categStr += categories[i] + ";";
+        }
+        monWriter.println("i" + totalTicket + ";" + super.getCompteID() + ";" + super.getEmail() +  ";" + description + ";" + categStr + true);
+        monWriter.close();
+        System.out.println("Ecrit dans ticket.txt : i" + totalTicket + ";" + super.getCompteID() + ";" + super.getEmail() +  ";" + description + ";" + categories + true);
+          
+    	} catch (IOException e) {
+		    System.out.println("Il y a eu une erreur.");
+		    e.printStackTrace();
+		  }
+  }
 
 }
