d6b8e1d1c7d4d3f114cef539b3e254b93b53b249
Jean-Gab <roujea08@ecolecatholique.ca>
2026-05-14 13:39:57 -0400

    GUIit friggin works babyyyyyyyyyy

--- TestProject/GUIit.java
+++ TestProject/GUIit.java
@@ -1,84 +1,91 @@
 import java.awt.*;
 import javax.swing.*;
 import java.awt.event.*;
-public class GUIit extends JFrame {
-JPanel rightPanel = new JPanel();
-JPanel topPanel = new JPanel();
-JPanel leftPanel = new JPanel();
-JPanel rightBottomPanel = new JPanel();
-JTextField leftTop = new JTextField();
-JTextField left1 = new JTextField();
-JTextField left2 = new JTextField();
-JTextField left3 = new JTextField();
-JTextField left4 = new JTextField();
-JTextField left5 = new JTextField();
-JTextPane test = new JTextPane();
-JTextPane description = new JTextPane();
-JTextArea notes = new JTextArea();
-JScrollPane scrollPane = new JScrollPane(notes);
-JLabel titreTicket = new JLabel();
-JButton confirmeResolution = new JButton();
-JLabel[] infos = new JLabel[6];
 
-GridLayout baseGrid = new GridLayout(7,1);
-FlowLayout flowthing = new FlowLayout();
+public class GUIit extends JFrame implements ActionListener{
+	JPanel rightPanel = new JPanel();
+	JPanel topPanel = new JPanel();
+	JPanel leftPanel = new JPanel();
+	JPanel rightBottomPanel = new JPanel();
+	JTextField leftTop = new JTextField();
+	JTextField left1 = new JTextField();
+	JTextField left2 = new JTextField();
+	JTextField left3 = new JTextField();
+	JTextField left4 = new JTextField();
+	JTextField left5 = new JTextField();
+	JTextPane test = new JTextPane();
+	JTextPane description = new JTextPane();
+	JTextArea notes = new JTextArea();
+	JScrollPane scrollPane = new JScrollPane(notes);
+	JLabel titreTicket = new JLabel();
+	JButton confirmeResolution = new JButton();
+	JLabel[] infos = new JLabel[6];
 
-public GUIit () {
-super ("GUIit");
-setSize(500,500);
-getContentPane().setLayout(new GridLayout(1,2));
-this.add(leftPanel);
-this.add(rightPanel);
-   rightPanel.setBackground(Color.yellow);
-rightBottomPanel.setBackground(Color.blue);
-rightBottomPanel.setLayout(new GridLayout(1,2));
-left3.setBackground(Color.RED);
-leftTop.setBackground(Color.green);
-leftPanel.setLayout(new GridLayout(6,1));
-leftPanel.add(leftTop);
-leftPanel.add(left1);
-leftPanel.add(left2);
-leftPanel.add(left3);
-leftPanel.add(left4);
-leftPanel.add(left5);
+	GridLayout baseGrid = new GridLayout(7,1);
+	FlowLayout flowthing = new FlowLayout();
+	
+	TicketReparation thingy = new TicketReparation();
 
-rightPanel.setLayout(new GridLayout(8,1));
-for(int i = 0; i<6; i++) {
-infos[i] = new JLabel();
-}
-infos[0].setText("Les problèmes");
-infos[1].setText("Des");
-infos[2].setText("Élèves");
-infos[3].setText("Serons");
-infos[4].setText("Affichés");
-infos[5].setText("Ici!");
-for(int i = 0; i<6; i++) {
-rightPanel.add(infos[i]);
-}
+	public GUIit () {
+			super ("GUIit");
+			setSize(500,500);
+			getContentPane().setLayout(new GridLayout(1,2));
+			this.add(leftPanel);
+			this.add(rightPanel);
+			rightPanel.setBackground(Color.yellow);
+			rightBottomPanel.setBackground(Color.blue);
+			rightBottomPanel.setLayout(new GridLayout(1,2));
+			left3.setBackground(Color.RED);
+			leftTop.setBackground(Color.green);
+			leftPanel.setLayout(new GridLayout(6,1));
+			leftPanel.add(leftTop);
+			leftPanel.add(left1);
+			leftPanel.add(left2);
+			leftPanel.add(left3);
+			leftPanel.add(left4);
+			leftPanel.add(left5);
+
+			rightPanel.setLayout(new GridLayout(8,1));
+			for(int i = 0; i<6; i++) {
+				infos[i] = new JLabel();
+			}
+			infos[0].setText("Problème d'alimentation : ");
+			infos[1].setText("Des");
+			infos[2].setText("Élèves");
+			infos[3].setText("Serons");
+			infos[4].setText("Affichés");
+			infos[5].setText("Ici!");
+			for(int i = 0; i<6; i++) {
+				rightPanel.add(infos[i]);
+			}
 
 
-description.setEditable(false);
-rightPanel.add(description);
-notes.setLineWrap(true);
-rightBottomPanel.add(scrollPane);
-rightBottomPanel.add(confirmeResolution);
-rightPanel.add(rightBottomPanel);
-leftPanel.setBackground(Color.green);
-rightPanel.setBackground(Color.cyan);
-
-setVisible(true);
-}
-
-	public void actionPerformed (ActionEvent actionEvent ) {
-		
+			description.setEditable(false);
+			rightPanel.add(description);
+			notes.setLineWrap(true);
+			rightBottomPanel.add(scrollPane);
+			rightBottomPanel.add(confirmeResolution);
+			confirmeResolution.addActionListener(this);
+			confirmeResolution.setText("Soummetre la résolution");
+			rightPanel.add(rightBottomPanel);
+			leftPanel.setBackground(Color.green);
+			rightPanel.setBackground(Color.cyan);
+			setVisible(true);
 	}
 
-
-
-
-
-
-
-
+	public void actionPerformed (ActionEvent actionEvent ) {
+		String[] affichage = {"Problème d'alimentation : ", "Câble brisé : ","Écran brisé : ", "Batterie défectueuse : ", "Ne s'allume pas : ", "Problème logiciel : "};
+		if (actionEvent.getSource() == confirmeResolution) {
+			thingy.lireTicketFicher ("i1");
+			boolean[] affiche = thingy.getCategories();
+			System.out.println(affiche[1]);
+			for (int i = 0; i <6; i++) {
+				infos[i].setText(String.valueOf(affichage[i] + affiche[i]));
+				
+			}
+			description.setText(thingy.getDescription());
+		}
+	}
 
 }
+	
