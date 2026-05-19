00e3eef95cc12f315c5db296a5ede07df8245eb8
Evan <mereva29@ecolecatholique.ca>
2026-05-19 14:10:54 -0400

    GUIit now only through GUIcreation

--- TestProject/ObjWithMainToRunStuff.java
+++ TestProject/ObjWithMainToRunStuff.java
@@ -4,7 +4,6 @@ public class ObjWithMainToRunStuff {
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 		GUIcreation testing = new GUIcreation();
-		GUIit testing2 = new GUIit();
 	}
 
 }
